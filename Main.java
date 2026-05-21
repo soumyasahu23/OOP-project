5ff4b9a3f54cfde3b786781fda5cd3950d47258e
soumyasahu23 <soumyasahu06231109@gmail.com>
2026-05-21 06:02:13 +0430

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,56 @@
-import java.util.Scanner;
-//Car class
-class Car{
-    String vehicleNumber;
-    String brand;
-    int rentPerDay;
-    //Constructor
-    Car(String vehicleNumber, String brand, int rentPerDay){
-        this.vehicleNumber = vehicleNumber;
-        this.brand = brand;
-        this.rentPerDay = rentPerDay;
-    }
-    //Method to calculate rent
-    int calculateRent(int days){
-        return rentPerDay * days;
-    }
-}
-//Bike class
-class Bike{
-    String vehicleNumber;
-    String brand;
-    int rentPerDay;
-    //Constructor
-    Bike(String vehicleNumber, String brand, int rentPerDay){
-        this.vehicleNumber = vehicleNumber;
-        this.brand = brand;
-        this.rentPerDay = rentPerDay;
-    }
-    //Method to calculate rent
-    int calculateRent(int days){
-        return rentPerDay * days;
-    }
-}
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        //Parent class references
+        //Polymorphism
+        Vehicle v1 = new Car("CG 04 AB 1023", "Toyota Corolla", 2500);
+        Vehicle v2 = new Bike("CG 04 MN 4455", "Royal Enfield", 1800);
 
-//Main class
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        //Taking input
-        System.out.print("Enter number of days: ");
+        System.out.println("Enter no. of days you want to rent Bike or Car: ");
         int days = sc.nextInt();
-        //Creating objects
-        Car c1 = new Car(vehicleNumber: "C101", brand: "Toyota", rentPerDay: 1000);
-        Bike b1 = new Bike(vehicleNumber: "B101", brand: "Honda", rentPerDay: 300);
-        System.out.println("Car Rent for "+ days + " days: " + c1.calculateRent(days));
-        System.out.println("Bike Rent for "+ days + " days: " + b1.calculateRent(days));
+        System.out.println("Car Rent for 3 days: " + v1.calculateRent(days));
+        System.out.println("Bike Rent for 3 days: " + v2.calculateRent(days));
     }
 }
-        
+class Vehicle {
+    String vehicleNumber;
+    String brand;
+    int rentPerDay;
+
+    //Constructor
+    Vehicle(String vehicleNumber, String brand, int rentPerDay) {
+        this.vehicleNumber = vehicleNumber;
+        this.brand = brand;
+        this.rentPerDay = rentPerDay;
+    }
+
+    //Method to calculate rent
+    int calculateRent(int days) {
+        return rentPerDay * days;
+    }
+}
+class Car extends Vehicle {
+
+    //Constructor
+    Car(String vehicleNumber, String brand, int rentPerDay) {
+        super(vehicleNumber, brand, rentPerDay);
+    }
+
+    //Overriding method
+    int calculateRent(int days) {
+        return rentPerDay * days;
+    }
+}
+class Bike extends Vehicle {
+
+    //Constructor
+    Bike(String vehicleNumber, String brand, int rentPerDay) {
+        super(vehicleNumber, brand, rentPerDay);
+    }
+
+    //Overriding method
+    int calculateRent(int days) {
+        return rentPerDay * days;
+    }
+}
